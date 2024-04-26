6698d6b03c33e45cef1bb4f32b8ee889ff37f7af
Adrian Galvez <l20441114@mochis.tecnm.mx>
2024-04-26 00:42:21 -0700

    creacion de vistas, controladores y modelos preparados para su programacion

---
+++ app/Controllers/Crud.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controllers;
+
+class Crud extends BaseController
+{
+    public function index(): string
+    {
+        return view('registro');
+    }
+
+    public function crear(){
+
+    }
+
+    public function actualizar(){
+
+    }
+
+    public function consultar(){
+
+    }
+
+    public function obtenerNombre(){
+        
+    }
+}
